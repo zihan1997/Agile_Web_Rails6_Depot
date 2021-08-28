829661a084fb21ce2c08d69eb0e89c01a65f845e
Zihan Wang <zihan9701@gmail.com>
2021-08-28 23:26:25 +0800

    unit testing of models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  # all not empty
+  validates :title, :description, :image_url, presence: true
+
+  # title be unique
+  validates :title, uniqueness: true
+
+  # image meets format
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG, or PNG image.'
+  }
+
+  # price >= 0.01
+  validates :price, numericality: { greater_than_or_equal_to: 0.01}
+
+
 end
