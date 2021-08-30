bd039bb23e0f7add46808638eb278a9afc3bb437
Zihan Wang <zihan9701@gmail.com>
2021-08-30 22:56:04 +0800

    Task D: Cart Creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,30 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  #...
+
   # all not empty
   validates :title, :description, :image_url, presence: true
-
   # title be unique
   validates :title, uniqueness: true
-
   # image meets format
   validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|png)\Z}i,
+    with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG, or PNG image.'
   }
-
   # price >= 0.01
   validates :price, numericality: { greater_than_or_equal_to: 0.01}
 
+
+  private
+
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 
 end
