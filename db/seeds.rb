3c4cbb2c89720c07fa82056c536adc17ef3fc947
Zihan Wang <zihan9701@gmail.com>
2021-08-30 10:53:35 +0800

    Task C: catalog display

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ Product.create!(
   price: '20.00')
 
   Product.create!(
-    title:'Build Chatbot Interactions',
+    title:'Docker for Rails Developers',
     description:%{<p>
   ​ 	      <em>Responsive, Intuitive Interfaces with Ruby</em>
   ​ 	      The next step in the evolution of user interfaces is here.
@@ -37,5 +37,22 @@ Product.create!(
   ​ 	      can participate. Exercise your creativity by creating chatbot
   ​ 	      skills for communicating, information, and fun.
   ​ 	      </p>},
-    image_url: '7apps.jpg',
+    image_url: 'ridocker.jpg',
     price: '10.00')
+
+    Product.create!(
+      title:'Programming Crystal',
+      description:%{<p>
+    ​ 	      <em>Responsive, Intuitive Interfaces with Ruby</em>
+    ​ 	      The next step in the evolution of user interfaces is here.
+    ​ 	      Chatbots let your users interact with your service in their
+    ​ 	      own natural language. Use free and open source tools along
+    ​ 	      with Ruby to build creative, useful, and unexpected interactions
+    ​ 	      for users. Take advantage of the Lita framework’s step-by-step
+    ​ 	      implementation strategy to simplify bot development and testing.
+    ​ 	      From novices to experts, chatbots are an area in which everyone
+    ​ 	      can participate. Exercise your creativity by creating chatbot
+    ​ 	      skills for communicating, information, and fun.
+    ​ 	      </p>},
+      image_url: 'crystal.jpg',
+      price: '40.00')
